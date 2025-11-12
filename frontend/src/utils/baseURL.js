b5dc0f4fb471c174a49fa2cee5657a00c64fd338
Durgesh Kumar <codeforge.code@gmail.com>
2025-11-12 14:13:43 +0530

    Update baseURL.js

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,3 +1,3 @@
-export const Shopkeeper_Base_URL = "https://dealsbasket.shop/api/shopkeeper/";
-export const Base_URL = "https://dealsbasket.shop/api/";
-export const LOCALHOST_BASE_URL = "https://dealsbasket.shop/api/";
+export const Shopkeeper_Base_URL = "http://dealsbasket.shop/api/shopkeeper/";
+export const Base_URL = "http://dealsbasket.shop/api/";
+export const LOCALHOST_BASE_URL = "http://dealsbasket.shop/api/";
